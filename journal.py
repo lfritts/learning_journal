9ab8c4f4271d65964fd50b1be958912adafb0543
lfritts <llfritts@gmail.com>
2014-06-03 14:04:31 -0700

    Add db connection functions

--- journal.py
+++ journal.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 import os
+import psycopg2
+from contextlib import closing
 
 DB_SCHEMA = """
 DROP TABLE IF EXISTS entries;
@@ -16,6 +18,21 @@ app = Flask(__name__)
 app.config['DATABASE'] = os.environ.get(
     'DATABASE_URL', 'dbname=learning_journal user=miked, lfritts'
 )
+
+
+def connect_db():
+    """Return a connection to the configured database"""
+    return psycopg2.connect(app.config['DATABASE'])
+
+
+def init_db():
+    """Initialize the database using DB_SCHEMA
+
+    WARNING: executing this function will drop existing tables.
+    """
+    with closing(connect_db()) as db:
+        db.cursor().execute(DB_SCHEMA)
+        db.commit()
 
 
 @app.route('/')
