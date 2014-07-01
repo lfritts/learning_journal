0fae19cee56b95fcbb995750242beba1d48060f3
lfritts <llfritts@gmail.com>
2014-06-30 19:30:18 -0700

    Add ajax js to place an add or edit entry into html

---
+++ static/myAjax.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.add_entry').on('submit', function(addEntry){
+        addEntry.preventDefault();
+        $.ajax('/add', {
+            type: 'POST',
+            data: $('form').serialize(),
+            success: function(entryData) {
+                $('.newEntry').html(entryData);
+                $('.add_entry').remove();
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('.edit').on('submit', function(changeEntry){
+        changeEntry.preventDefault();
+        $.ajax('/update_entry', {
+            type: 'POST',
+            data: $('form').serialize(),
+            success: function(entryData) {
+                $('.newEntry').html(entryData);
+                $('.edit').remove();
+            }
+        });
+    });
+});
